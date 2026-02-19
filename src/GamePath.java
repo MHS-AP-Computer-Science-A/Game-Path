2575acdc1f9685d3d570470fd65175c655b6eced
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-02-19 08:19:16 -0500

    Add files via upload

---
+++ src/GamePath.java
@@ -0,0 +1,56 @@
+public class GameGrid {
+    /**
+     * A grid that a game piece moves on. Each element indicates the
+     * direction the game piece should move next (either "NE", "NW",
+     * "SE", or "SW").
+     */
+    private String[][] grid;
+
+    public GameGrid(String[][] grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * Computes and returns the number of moves that are taken
+     * by the game piece when following a path through the grid
+     * Preconditions: startRow is a valid row index.
+     * startCol is a valid column index.
+     * There exists a path that moves the game piece
+     * from the starting position to past the
+     * boundaries of the grid.
+     * Postcondition: grid is unchanged.
+     */
+    public int navigate(int startRow, int startCol) { /* to be implemented */
+        int length = 1;
+
+        while (true) {
+            if (grid[startRow][startCol].equals("NE")) {
+                startRow--;
+                startCol++;
+            }
+            else if (grid[startRow][startCol].equals("NW")) {
+                startRow--;
+                startCol--;
+            }
+            else if (grid[startRow][startCol].equals("SE")) {
+                startRow++;
+                startCol++;
+            }
+            else if (grid[startRow][startCol].equals("SW")) {
+                startRow++;
+                startCol--;
+            }
+            if(startRow < 0 || startRow >= grid.length || startCol < 0 || startCol >= grid[0].length){
+                return length;
+            }
+            length++;
+
+        }
+
+    }
+
+    /*
+     * There may be instance variables, constructors,
+     * and methods that are not shown.
+     */
+}
---
+++ src/GamePathTester.java
@@ -0,0 +1,53 @@
+public class GameGridTester {
+ public static void main(String[] args)
+    {
+        // Create the example grid from the problem
+        String[][] data = {
+            {"SE","SE","NE","NW","NW","SW","SE"},
+            {"NE","SE","NE","SE","NE","SE","NE"},
+            {"NW","NW","NE","NW","SE","SW","SE"},
+            {"SE","SW","SE","SW","NE","SW","NW"},
+            {"SW","NE","NW","NE","SE","NE","SW"}
+        };
+
+        GameGrid grid = new GameGrid(data);
+
+
+        int result1 = grid.navigate(0, 0);
+        System.out.println("Start at (0,0)");
+        System.out.println("Expected: 7");
+        System.out.println("Your result: " + result1);
+        System.out.println();
+
+        int result2 = grid.navigate(2, 1);
+        System.out.println("Start at (2,1)");
+        System.out.println("Expected: 5");
+        System.out.println("Your result: " + result2);
+        System.out.println();
+
+
+        // Edge start near bottom-left
+        int result3 = grid.navigate(4, 0);
+        System.out.println("Start at (4,0)");
+        System.out.println("Expected: 1");
+        System.out.println("Your result: " + result3);
+        System.out.println();
+
+        // Edge start near top-right
+        int result4 = grid.navigate(3, 1);
+        System.out.println("Start at (3,1)");
+        System.out.println("Expected: 2");
+        System.out.println("Your result: " + result4);
+        System.out.println();
+
+        // Middle start
+        int result5 = grid.navigate(3, 3);
+        System.out.println("Start at (3,3)");
+        System.out.println("Expected: 4");
+        System.out.println("Your result: " + result5);
+        System.out.println();
+
+
+        System.out.println("===== DONE =====");
+    }
+}
